82d936e10d7dbdc1344f2f1e665be496c635c70d
sriman <sriman456.singh@gmail.com>
2022-11-21 18:18:46 +0800

    upgraded the jar version and added CORS ignore code

--- src/main/java/com/halftusk/authentication/authservice/config/SecurityConfiguration.java
+++ src/main/java/com/halftusk/authentication/authservice/config/SecurityConfiguration.java
@@ -11,26 +11,34 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Collections;
 
 @Configuration
 @Order(1)
+@EnableGlobalAuthentication
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private UsernamePasswordAuthProvider authProvider;
+    private final UsernamePasswordAuthProvider authProvider;
 
-    @Autowired
-    private OtpAuthenticationProvider otpAuthenticationProvider;
+    private final OtpAuthenticationProvider otpAuthenticationProvider;
 
-    @Autowired
-    private TokenAuthProvider tokenAuthProvider;
+    private final TokenAuthProvider tokenAuthProvider;
+
+    public SecurityConfiguration(UsernamePasswordAuthProvider authProvider, OtpAuthenticationProvider otpAuthenticationProvider,
+                                 TokenAuthProvider tokenAuthProvider) {
+        this.authProvider = authProvider;
+        this.otpAuthenticationProvider = otpAuthenticationProvider;
+        this.tokenAuthProvider = tokenAuthProvider;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -72,6 +80,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("**/**").allowedOrigins("*");
+            }
+        };
     }
 
     @Bean
--- src/main/java/com/halftusk/authentication/authservice/controller/TestController.java
+++ src/main/java/com/halftusk/authentication/authservice/controller/TestController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class TestController {
 	@GetMapping("/all")
 	public String allAccess() {
